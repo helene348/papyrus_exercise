d5f37be8abbacdd538ebb8065f95083a145067e4
Hélène <helen-r@hotmail.fr>
2021-03-07 20:04:57 +0100

    ajouter (), fonctionne dans dbeaver

--- papyrus.sql
+++ papyrus.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS "fournis";
 DROP TABLE IF EXISTS "produit";
 
 
-CREATE TABLE "produit" {
+CREATE TABLE "produit" (
     "codart" CHAR(4) UNIQUE NOT NULL,
     "libart" VARCHAR(25) NOT NULL,
     "stkale" SMALLINT NOT NULL,
@@ -13,10 +13,10 @@ CREATE TABLE "produit" {
     "qteann" SMALLINT NOT NULL,
     "unimes" CHAR(5) NOT NULL,
     PRIMARY KEY("codart")
-};
+);
 
-CREATE TABLE "fournis" {
-    "numfou" UNIQUE NOT NULL,
+CREATE TABLE "fournis" (
+    "numfou" INT UNIQUE NOT NULL,
     "nomfou" VARCHAR(30) NOT NULL,
     "ruefou" VARCHAR(25) NOT NULL,
     "posfou" CHAR(5) NOT NULL,
@@ -24,40 +24,39 @@ CREATE TABLE "fournis" {
     "confou" VARCHAR(15) NOT NULL,
     "satisf" SMALLINT,
     PRIMARY KEY("numfou")
-};
+);
 
-CREATE TABLE "entcom" {
-    "numcom" INT NOT NULL,
+CREATE TABLE "entcom" (
+    "numcom" INT UNIQUE NOT NULL,
     "obscom" VARCHAR(25),
     "datcom" DATE NOT NULL,
     "numfou" INT NOT NULL,
     PRIMARY KEY("numcom")
-};
+);
 
-CREATE TABLE "ligcom" {
-    "numcom" NOT NULL,
-    "codart" NOT NULL,
+CREATE TABLE "ligcom" (
+    "numcom" INT UNIQUE NOT NULL,
+    "codart" CHAR(4) UNIQUE NOT NULL,
     "numlig" SMALLINT NOT NULL,
     "qtecde" SMALLINT NOT NULL,
-    "priuni" NUMBER(7,2) NOT NULL,
+    "priuni" NUMERIC(7,2) NOT NULL,
     "qteliv" SMALLINT,
     "derliv" DATE NOT NULL,
     PRIMARY KEY("numcom", "codart"),
     FOREIGN KEY("numcom") REFERENCES "entcom"("numcom"),
     FOREIGN KEY("codart") REFERENCES "produit"("codart")
-};
+);
 
-CREATE TABLE "vente" {
+CREATE TABLE "vente" (
     "codart" CHAR(4) UNIQUE NOT NULL,
-    "numfou" NOT NULL,
+    "numfou" INT UNIQUE NOT NULL,
     "delliv" SMALLINT NOT NULL,
     "qte1" SMALLINT NOT NULL,
-    "prix1" NUMBER(7,2) NOT NULL,
+    "prix1" NUMERIC(7,2) NOT NULL,
     "qte2" SMALLINT,
-    "prix2" NUMBER(7,2),
+    "prix2" NUMERIC(7,2),
     "qte3" SMALLINT,
-    "prix3" NUMBER(7,2),
+    "prix3" NUMERIC(7,2),
     PRIMARY KEY("codart", "numfou"),
-    FOREIGN KEY("numfou") REFERENCES "vente"("numfou"),
-    FOREIGN KEY("codart") REFERENCES "produit"("codart")
-};
+    FOREIGN KEY("numfou") REFERENCES "vente"("numfou")
+);
